1b10783b0bb74609ca396ef3192575e78b71ed72
afirahzahid <43724763+afirahzahid@users.noreply.github.com>
2019-03-07 01:14:31 +0500

    2nd commit

--- Dblab/Dblab/Form1.Designer.cs
+++ Dblab/Dblab/Form1.Designer.cs
@@ -32,21 +32,28 @@
 			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.lala = new System.Windows.Forms.Label();
+			this.TotalMarks = new System.Windows.Forms.Label();
+			this.TotalWeightage = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// dataGridView1
 			// 
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(75, 168);
+			this.dataGridView1.Location = new System.Drawing.Point(75, 194);
 			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(615, 230);
+			this.dataGridView1.Size = new System.Drawing.Size(615, 204);
 			this.dataGridView1.TabIndex = 0;
 			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(542, 122);
+			this.button1.Location = new System.Drawing.Point(491, 154);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 1;
@@ -56,19 +63,89 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(418, 122);
+			this.button2.Location = new System.Drawing.Point(345, 154);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
 			this.button2.TabIndex = 2;
-			this.button2.Text = "Display";
+			this.button2.Text = "Delete";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(188, 154);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Insert";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// lala
+			// 
+			this.lala.AutoSize = true;
+			this.lala.Location = new System.Drawing.Point(152, 52);
+			this.lala.Name = "lala";
+			this.lala.Size = new System.Drawing.Size(23, 13);
+			this.lala.TabIndex = 5;
+			this.lala.Text = "lala";
+			this.lala.Click += new System.EventHandler(this.Name_Click);
+			// 
+			// TotalMarks
+			// 
+			this.TotalMarks.AutoSize = true;
+			this.TotalMarks.Location = new System.Drawing.Point(152, 85);
+			this.TotalMarks.Name = "TotalMarks";
+			this.TotalMarks.Size = new System.Drawing.Size(60, 13);
+			this.TotalMarks.TabIndex = 6;
+			this.TotalMarks.Text = "TotalMarks";
+			this.TotalMarks.Click += new System.EventHandler(this.TotalMarks_Click);
+			// 
+			// TotalWeightage
+			// 
+			this.TotalWeightage.AutoSize = true;
+			this.TotalWeightage.Location = new System.Drawing.Point(152, 116);
+			this.TotalWeightage.Name = "TotalWeightage";
+			this.TotalWeightage.Size = new System.Drawing.Size(83, 13);
+			this.TotalWeightage.TabIndex = 7;
+			this.TotalWeightage.Text = "TotalWeightage";
+			this.TotalWeightage.Click += new System.EventHandler(this.TotalWeightage_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(360, 45);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 9;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(360, 78);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(360, 109);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 11;
+			this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
 			// 
 			// Evaluation
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.TotalWeightage);
+			this.Controls.Add(this.TotalMarks);
+			this.Controls.Add(this.lala);
+			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.dataGridView1);
@@ -77,6 +154,7 @@
 			this.Load += new System.EventHandler(this.Evaluation_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -86,6 +164,13 @@
 		private System.ComponentModel.BackgroundWorker backgroundWorker1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label lala;
+		private System.Windows.Forms.Label TotalMarks;
+		private System.Windows.Forms.Label TotalWeightage;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
 	}
 }
 
--- Dblab/Dblab/Form1.cs
+++ Dblab/Dblab/Form1.cs
@@ -13,9 +13,10 @@ namespace Dblab
 {
 	public partial class Evaluation : Form
 	{
-		SqlDataAdapter sda;
-		SqlCommandBuilder scb;
-		DataTable dt;
+		SqlCommand cmd;
+		SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-KSK1C2C\SQLEXPRESS;Initial Catalog=ProjectA;Integrated Security=True");
+		SqlDataAdapter adp;
+		int ID = 0;
 		public Evaluation()
 		{
 			InitializeComponent();
@@ -23,22 +24,120 @@ namespace Dblab
 
 		private void Evaluation_Load(object sender, EventArgs e)
 		{
-
+			Display();
+		}
+		private void Display()
+		{
+			con.Open();
+			DataTable dt = new DataTable();
+			adp = new SqlDataAdapter("SELECT * FROM Evaluation", con);
+			adp.Fill(dt);
+			dataGridView1.DataSource = dt;
+			con.Close();
 		}
 
 		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
 		{
-
+			//MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+			ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+			textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+			textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+			textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
+			if (textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+			{
+				cmd = new SqlCommand("UPDATE Evaluation set Name = @name, TotalMarks = @TotalMarks, Totalweightage = @Totalweightage where ID = @id", con);
+				cmd.Parameters.AddWithValue("@id", ID);
+				cmd.Parameters.AddWithValue("@name", textBox2.Text); 
+				cmd.Parameters.AddWithValue("@Totalweightage", textBox3.Text);
+				cmd.Parameters.AddWithValue("@TotalMarks", textBox4.Text);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				con.Close();
+				MessageBox.Show("Updated successfully");
+				Display();
+			}
+			else
+			{
+				MessageBox.Show("Select data");
+			}
 
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-			SqlConnection con = new SqlConnection("Data Source=DESKTOP-KSK1C2C\SQLEXPRESS;Initial Catalog=ProjectA;Integrated Security=True");
+			if (ID != 0)
+			{
+				cmd = new SqlCommand("DELETE FROM Evaluation where ID = @Id", con);
+				con.Open();
+				cmd.Parameters.AddWithValue("@Id", ID);
+				cmd.ExecuteNonQuery();
+				con.Close();
+				MessageBox.Show("Delete successfully");
+				Display();
+				textBox2.Clear();
+				textBox3.Clear();
+				textBox4.Clear();
+			}
+			else
+			{
+				MessageBox.Show("Select Row to delete");
+			}
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+			{
+				cmd = new SqlCommand("insert into Evaluation (Evaluation.Name, TotalMarks, TotalWeightage) values ('" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "') ", con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				con.Close();
+				MessageBox.Show("Inserted Successfully");
+				Display();
+			}
+			else
+			{
+				MessageBox.Show("Please enter record");
+			}
+		}
+
+		private void ID_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Name_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void TotalMarks_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void TotalWeightage_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox4_TextChanged(object sender, EventArgs e)
+		{
 
 		}
 	}
