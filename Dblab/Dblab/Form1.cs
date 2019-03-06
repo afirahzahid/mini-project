b84b2df99e79a64bbdea81cb5ef7df30cb94941d
afirahzahid <43724763+afirahzahid@users.noreply.github.com>
2019-03-06 22:28:20 +0500

    1st commit

---
+++ Dblab/Dblab/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Dblab
+{
+	public partial class Evaluation : Form
+	{
+		SqlDataAdapter sda;
+		SqlCommandBuilder scb;
+		DataTable dt;
+		public Evaluation()
+		{
+			InitializeComponent();
+		}
+
+		private void Evaluation_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SqlConnection con = new SqlConnection("Data Source=DESKTOP-KSK1C2C\SQLEXPRESS;Initial Catalog=ProjectA;Integrated Security=True");
+
+		}
+	}
+}
